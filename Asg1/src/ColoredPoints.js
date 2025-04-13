668967474b73b787f20be6f01622fe471c8ee5db
Romnr <148321973+Romnr@users.noreply.github.com>
2025-04-12 18:18:55 -0700

    more changes

--- Asg1/src/ColoredPoints.js
+++ Asg1/src/ColoredPoints.js
@@ -71,25 +71,18 @@ function main() {
 
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
-function click(ev) {
+
+function convertCoordinatesEventToGL(ev){
   var x = ev.clientX; // x coordinate of a mouse pointer
   var y = ev.clientY; // y coordinate of a mouse pointer
   var rect = ev.target.getBoundingClientRect();
 
   x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
   y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+  return ([x,y]);
+}
 
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
-  // Store the coordinates to g_points array
-  if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  } else {                         // Others
-    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  }
-
+function renderAllShapes(){
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -106,3 +99,22 @@ function click(ev) {
     gl.drawArrays(gl.POINTS, 0, 1);
   }
 }
+function click(ev) {
+
+  //Getting the WEBGL Coordinates
+  let [x,y]=convertCoordinatesEventToGL(ev); //made it local
+
+  // Store the coordinates to g_points array
+  g_points.push([x, y]);
+  // Store the coordinates to g_points array
+  if (x >= 0.0 && y >= 0.0) {      // First quadrant
+    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+  } else if (x < 0.0 && y < 0.0) { // Third quadrant
+    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+  } else {                         // Others
+    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+  }
+  
+  renderAllShapes();
+}
+
